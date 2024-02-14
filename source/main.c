02692273231c76d34ac17624c0077fa53dd17720
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-02-13 19:04:42 -0500

    feat: Add linear function
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- source/main.c
+++ source/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) {
+double linear(double x, double m, double b) { return m * x + b; }
 
-    printf("Hello world!\n");
-
+int main() {
+    double x = 3.0;
+    double m = 2.0;
+    double b = 1.0;
+    double y = linear(x, m, b);
+    printf("For x = %.2f, y = %.2f\n", x, y);
     return 0;
 }
