72f6226529089a3624e5c6a51623ecbfcdb042f1
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-02-14 23:46:25 -0500

    refactor: Clean up code and fix logger initialization
    - Consolidated variable declaration and initialization for improved readability.
    - Adjusted function parameter list and removed unnecessary line breaks.
    - Modified register_parent_glfw_window_with_imgui to return ImGuiIO* for improved flexibility.
    - Added return statement in register_parent_glfw_window_with_imgui to return ImGuiIO*.
    - Updated main function to correctly initialize the logger with create_logger.
    - Updated log message in main function to use %s format specifier for generated text.
    - Ensured consistency in coding style and improved code organization.
    
    These changes enhance code clarity and ensure proper logger initialization, leading to improved maintainability and readability.

--- source/alt.cpp
+++ source/alt.cpp
@@ -1,50 +1,78 @@
+/*
+ * Copyright © 2024 Austin Berrio
+ *
+ * alt.cpp/source/alt.cpp
+ */
 #include "../imgui/backends/imgui_impl_glfw.h"
 #include "../imgui/backends/imgui_impl_opengl3.h"
 #include "../imgui/imgui.h"
+#include "../include/log.h"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <cstdlib>
 #include <iostream>
 #include <stdio.h>
 
-int main(void) {
+GLFWwindow* create_parent_glfw_window(
+    int width, int height, const char* title, GLFWmonitor* monitor, struct Logger* logger
+) {
     if (!glfwInit()) {
-        fprintf(stderr, "Failed to initialize GLFW\n");
-        return -1;
+        LOG(logger, LOG_LEVEL_ERROR, "Failed to initialize GLFW\n");
+        return NULL;
     }
 
-    GLFWwindow* window
-        = glfwCreateWindow(1280, 720, "ImGui GLFW OpenGL3 example", NULL, NULL);
+    GLFWwindow* parent_window = glfwCreateWindow(width, height, title, monitor, NULL);
 
-    if (window == NULL) {
-        fprintf(stderr, "Failed to create a GLFW window\n");
-        glfwTerminate();
-        return -1;
+    if (parent_window == NULL) {
+        LOG(logger, LOG_LEVEL_ERROR, "Failed to create a GLFW window\n");
+        return NULL;
     }
 
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(parent_window);
     glfwSwapInterval(0); // Enable vsync
 
     if (glewInit() != GLEW_OK) {
-        fprintf(stderr, "Failed to initialize GLEW\n");
-        glfwTerminate();
-        return -1;
+        LOG(logger, LOG_LEVEL_ERROR, "Failed to initialize GLEW\n");
+        return NULL;
     }
 
+    return parent_window;
+}
+
+// Adding the return type as a pointer to ImGuiIO
+ImGuiIO* register_parent_glfw_window_with_imgui(GLFWwindow* pwindow, struct Logger* logger) {
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO();
     ImGui::StyleColorsDark();
 
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplGlfw_InitForOpenGL(pwindow, true);
     ImGui_ImplOpenGL3_Init("#version 130");
+    LOG(logger, LOG_LEVEL_INFO, "ImGui successfully initialized!\n");
 
-    std::string prompt = "Once upon a time";
-    std::string generated_text
-        = " a prompt generation was faked by a program named Alt.";
-    std::cout << "Generated text: " << generated_text << std::endl;
+    return &io; // Returning the address for io
+}
 
-    while (!glfwWindowShouldClose(window)) {
+int main(void) {
+    // Create the logger; no file to avoid clutter during development.
+    struct Logger* logger = create_logger(LOG_LEVEL_DEBUG, LOG_TYPE_STREAM, NULL);
+
+    // create the glfw parent window to generate the root context.
+    GLFWwindow* parent_window = create_parent_glfw_window(1280, 720, "alt.cpp", NULL, logger);
+
+    if (NULL == parent_window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    register_parent_glfw_window_with_imgui(parent_window, logger);
+
+    std::string prompt         = "Once upon a time";
+    std::string generated_text = " a prompt generation was faked by a program named Alt.";
+
+    log_message(logger, LOG_LEVEL_INFO, "Generated text: %s\n", generated_text.c_str());
+
+    while (!glfwWindowShouldClose(parent_window)) {
         glfwPollEvents();
 
         ImGui_ImplOpenGL3_NewFrame();
@@ -61,19 +89,19 @@ int main(void) {
 
         ImGui::Render();
         int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glfwGetFramebufferSize(parent_window, &display_w, &display_h);
         glViewport(0, 0, display_w, display_h);
         glClearColor(0.1f, 0.1f, 0.1f, 1.00f);
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(parent_window);
     }
 
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
 
-    glfwDestroyWindow(window);
+    glfwDestroyWindow(parent_window);
     glfwTerminate();
     return 0;
 }
