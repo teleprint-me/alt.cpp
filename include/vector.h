f6a90a2921338a22e177fc086ea4f1854df31380
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-02-18 00:57:11 -0500

    docs: Add docstrings describing the objects and their related parameters
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- include/vector.h
+++ include/vector.h
@@ -13,30 +13,135 @@
 #ifndef ALT_VECTOR_H
 #define ALT_VECTOR_H
 
-#include "logger.h"
-
 #include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// Structures
+
+/**
+ * @brief Represents a vector in n-dimensional space.
+ */
 struct Vector {
-    float* elements; // n-dimensional vector space
-    size_t size;     // number of elements in vector space
+    /**
+     * @brief Pointer to the array storing the n-dimensional vector elements.
+     */
+    float* elements;
+
+    /**
+     * @brief Number of elements in the vector, representing its dimensionality.
+     */
+    size_t size;
 };
 
-// Vector lifecycle management
+// Life-cycle operations
+
+/**
+ * @brief Creates a new vector with the specified size and initializes its elements to zero.
+ *
+ * The `vector_create` function dynamically allocates memory for a new vector structure and its
+ * elements array. If memory allocation fails, an error message is logged, and the function returns
+ * NULL. Otherwise, the function initializes all elements of the vector to zero and sets its size.
+ *
+ * @param size The size of the vector to be created.
+ * @return A pointer to the newly created vector on success, or NULL if memory allocation failed.
+ */
 struct Vector* vector_create(size_t size);
+
+/**
+ * @brief Creates a deep copy of a vector with the specified elements and size.
+ *
+ * The `vector_deep_copy` function creates a new vector with the same size as the input elements
+ * array and copies the contents of the array into the newly allocated vector. If memory allocation
+ * fails, an error message is logged, and the function returns NULL.
+ *
+ * @param elements An array containing the elements of the vector to be copied.
+ * @param size The size of the vector to be copied.
+ * @return A pointer to the newly created deep copy of the vector on success, or NULL if memory
+ * allocation failed.
+ */
 struct Vector* vector_deep_copy(const float elements[], size_t size);
+
+/**
+ * @brief Creates a deep copy of an existing vector.
+ *
+ * The `vector_clone` function creates a deep copy of the input vector by calling `vector_deep_copy`
+ * with the elements and size of the input vector. If memory allocation fails during the cloning
+ * process, an error message is logged, and the function returns NULL.
+ *
+ * @param vector A pointer to the vector to be cloned.
+ * @return A pointer to the newly created deep copy of the vector on success, or NULL if memory
+ * allocation failed.
+ */
 struct Vector* vector_clone(const struct Vector* vector);
-bool           vector_destroy(struct Vector* vector);
+
+/**
+ * @brief Destroys a vector and frees its allocated memory.
+ *
+ * The `vector_destroy` function frees the memory allocated for the input vector and its elements
+ * array. If the input vector or its elements array is NULL, an error message is logged, and the
+ * function returns false.
+ *
+ * @param vector A pointer to the vector to be destroyed.
+ * @return true if the vector was successfully destroyed and memory freed, false otherwise.
+ */
+
+bool vector_destroy(struct Vector* vector);
 
 // Mathematical operations
-float          vector_magnitude(const struct Vector* vector);
+
+/**
+ * @brief Calculates the magnitude of a vector.
+ *
+ * The `vector_magnitude` function computes the Euclidean magnitude of the input vector by summing
+ * the squares of its elements and taking the square root of the result. This represents the length
+ * of the vector in n-dimensional space.
+ *
+ * @param vector A pointer to the vector whose magnitude is to be calculated.
+ * @return The magnitude of the input vector.
+ */
+float vector_magnitude(const struct Vector* vector);
+
+/**
+ * @brief Normalizes a vector to produce a unit vector.
+ *
+ * The `vector_normalize` function normalizes the input vector by dividing each of its elements by
+ * its magnitude, resulting in a unit vector with the same direction but a magnitude of 1. If the
+ * input vector has a magnitude of zero, an error message is logged, and NULL is returned.
+ *
+ * If `inplace` is true, the normalization is performed in-place, modifying the input vector. If
+ * `inplace` is false, a new vector is created to store the normalized result, leaving the input
+ * vector unchanged.
+ *
+ * @param vector A pointer to the vector to be normalized.
+ * @param inplace A boolean indicating whether to perform the normalization in-place or not.
+ * @return A pointer to the normalized unit vector on success, or NULL if memory allocation failed
+ * or if the input vector has zero magnitude.
+ */
 struct Vector* vector_normalize(struct Vector* vector, bool inplace);
-float          vector_distance(const struct Vector* a, const struct Vector* b);
+
+/**
+ * @brief Calculates the distance between two vectors.
+ *
+ * @param a The first vector.
+ * @param b The second vector.
+ * @return The distance between the two vectors.
+ */
+float vector_distance(const struct Vector* a, const struct Vector* b);
+
+/**
+ * @brief Scales a vector by a scalar value (optional in-place).
+ *
+ * @param vector The vector to scale.
+ * @param scalar The scalar value.
+ * @param inplace If true, scale the vector in-place.
+ * @return A pointer to the scaled vector.
+ */
 struct Vector* vector_scale(struct Vector* vector, float scalar, bool inplace);
+
+// Element wise operations
 
 // Helper functions for element-wise operations
 float add(float x, float y);
@@ -44,17 +149,84 @@ float subtract(float x, float y);
 float multiply(float x, float y);
 float divide(float x, float y);
 
-// Element wise operations
+/**
+ * @brief Performs an element-wise operation on two input vectors.
+ *
+ * The `perform_elementwise_operation` function takes two input vectors `a` and `b` and applies the
+ * specified element-wise operation to each corresponding pair of elements. The operation is defined
+ * by a function pointer `operation` which takes two floats and returns a float.
+ *
+ * If the sizes of the input vectors do not match, an error message is logged, and the function
+ * returns NULL. Otherwise, the function dynamically allocates memory for the resultant vector `c`
+ * and performs the element-wise operation, storing the results in `c`.
+ *
+ * @param a The first input vector.
+ * @param b The second input vector.
+ * @param operation A function pointer to the element-wise operation to be performed.
+ * @return A pointer to the resultant vector on success, or NULL if an error occurred.
+ */
 struct Vector* perform_elementwise_operation(
     const struct Vector* a, const struct Vector* b, float (*operation)(float, float)
 );
+
+// Element-wise operation wrappers
+
+/**
+ * @brief Performs element-wise addition between two n-dimensional vectors.
+ */
 struct Vector* vector_add(const struct Vector* a, const struct Vector* b);
+
+/**
+ * @brief Performs element-wise subtraction between two n-dimensional vectors.
+ */
 struct Vector* vector_subtract(const struct Vector* a, const struct Vector* b);
+
+/**
+ * @brief Performs element-wise multiplication between two n-dimensional vectors.
+ */
 struct Vector* vector_multiply(const struct Vector* a, const struct Vector* b);
+
+/**
+ * @brief Performs element-wise division between two n-dimensional vectors.
+ */
 struct Vector* vector_divide(const struct Vector* a, const struct Vector* b);
 
 // Vector products
-float          vector_dot_product(const struct Vector* a, const struct Vector* b);
+
+/**
+ * @brief Computes the dot product of two input vectors.
+ *
+ * The `vector_dot_product` function calculates the dot product of two input vectors `a` and `b`,
+ * which represents the cosine of the angle between the two vectors multiplied by their
+ * magnitudes. The dot product is computed by summing the products of the corresponding elements
+ * of the input vectors.
+ *
+ * If the sizes of the input vectors do not match, an error message is logged, and `NAN` (Not a
+ * Number) is returned.
+ *
+ * @param a A pointer to the first input vector.
+ * @param b A pointer to the second input vector.
+ * @return The dot product of the input vectors on success, or `NAN` if the input vector sizes do
+ * not match.
+ */
+float vector_dot_product(const struct Vector* a, const struct Vector* b);
+
+/**
+ * @brief Computes the cross product of two 3-dimensional vectors.
+ *
+ * The `vector_cross_product` function calculates the cross product of two input 3-dimensional
+ * vectors `a` and `b`, which represents a vector perpendicular to both input vectors. The cross
+ * product is only defined for 3-dimensional vectors.
+ *
+ * If either of the input vectors is not 3-dimensional, an error message is logged, and NULL is
+ * returned. Otherwise, the function allocates memory for the resulting cross product vector and
+ * computes its components based on the input vectors.
+ *
+ * @param a A pointer to the first input 3-dimensional vector.
+ * @param b A pointer to the second input 3-dimensional vector.
+ * @return A pointer to the resulting cross product vector on success, or NULL if the input vectors
+ * are not 3-dimensional or if memory allocation failed.
+ */
 struct Vector* vector_cross_product(const struct Vector* a, const struct Vector* b);
 
 // Coordinate transformations
@@ -62,10 +234,25 @@ struct Vector* vector_polar_to_cartesian(const struct Vector* polar_vector);
 struct Vector* vector_cartesian_to_polar(const struct Vector* cartesian_vector);
 
 // Advanced operations
-// NOTE: Consider using the "log-sum-exp" trick for numerical stability.
+
+/**
+ * @brief Computes the softmax function of a vector.
+ *
+ * The `vector_softmax` function calculates the softmax of the input vector, which is a
+ * normalization function that converts a vector of arbitrary real values into a vector of
+ * probabilities. The softmax function is computed by taking the exponential of each element in the
+ * input vector, dividing each exponential by the sum of all exponentials, and storing the result
+ * in the output vector.
+ *
+ * If memory allocation fails during the calculation process, NULL is returned. Otherwise, the
+ * function returns a pointer to the resulting softmax vector.
+ *
+ * @param vector A pointer to the input vector.
+ * @return A pointer to the resulting softmax vector on success, or NULL if memory allocation
+ * failed.
+ */
 struct Vector* vector_softmax(const struct Vector* vector);
-// Ensure it's clear that the vector_cross_entropy function expects the prediction vector to have
-// passed through a softmax or represents probabilities.
-float          vector_cross_entropy(const struct Vector* prediction, const struct Vector* target);
+
+float vector_cross_entropy(const struct Vector* prediction, const struct Vector* target);
 
 #endif // ALT_VECTOR_H
