bde402cd1f6306f87e0841d883da4472e4cb108e
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-08-13 21:17:47 -0400

    dag(feat): Add stub for dag related source code

---
+++ include/dag.h
@@ -0,0 +1,16 @@
+/**
+ * Copyright © 2024 Austin Berrio
+ *
+ * @file include/dag.h
+ *
+ * @brief Directed acyclic graph
+ *
+ * Only pure C is used with minimal dependencies on external libraries.
+ */
+
+#ifndef ALT_DAG_H
+#define ALT_DAG_H
+
+#include <stdlib.h>
+
+#endif // ALT_DAG_H
